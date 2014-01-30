2f947a1697b6974e50356e8c1738d4737ed6104b
Michael Yourshaw <myourshaw@ucla.edu>
2014-01-29 16:53:24 -0800

    hgmd

--- VAX_DIR/install_hgmd.sh
+++ VAX_DIR/install_hgmd.sh
@@ -3,7 +3,7 @@
 #ensembl_database_install_server.sh
 #runs on mysql server
 #assumes mysql files downloaded and unzipped
-USAGE="usage: $0 <hgmd directory> <hgmd version> <host> <port> <user> <password>"
+USAGE="usage: $0 <hgmd directory> <hgmd version> <host> <port> <user> [<password>]"
 if [[ $1 == "?" || $1 == "-h" || $1 == "--help" || $1 == "help" ]]; then echo ${USAGE}; exit; fi
  
 #get path and parent directory of this script
@@ -16,25 +16,42 @@ script_dir=$(dirname "$script_path");
 HERE=${script_dir};
 wd=`pwd`;
  
-if (( $# < 6 )) || [[ ! -d ${1} ]]; then echo ${USAGE}; exit; fi
+if (( $# < 5 )) || [[ ! -d ${1} ]]; then echo ${USAGE}; exit; fi
 
 HGMD_DIR=${1}; shift;
 VERSION=${1}; shift;
 HOST=${1}; shift;
 PORT=${1}; shift;
 USER=${1}; shift;
-PASSWORD=${1}; shift;
-MYSQL="$(which mysql) -h ${HOST} --user=${USER} -p${PASSWORD}";
-MYSQLIMPORT="mysqlimport -h ${HOST} --user=${USER} -p${PASSWORD}";
+if [ -z "${1}" ]; then
+	read -p "Enter MySQL password for database installation (or run using the -p option): " -s -r
+	if [ -z ${REPLY} ]
+	then
+		echo;
+		log "Installer aborted."
+		exit 1;
+	else
+		PASSWORD=${REPLY};
+	fi
+else
+  PASSWORD=${1}; shift;
+fi
+
+MYSQL="$(which mysql) -h ${HOST} -P ${PORT} --user=${USER} -p${PASSWORD}";
+MYSQLIMPORT="mysqlimport -h ${HOST} -P ${PORT} --user=${USER} -p${PASSWORD}";
 
 #drop/recreate databases and import data
-for DB in {hgmd_pro,hgmd_views,hgmd_snp,hgmd_phenbase}; do
+#hgmd_views should be last
+for DB in {hgmd_pro,hgmd_snp,hgmd_phenbase,hgmd_views}; do
+  echo "dropping and recreating ${DB}";
   ${MYSQL} -e "DROP DATABASE IF EXISTS ${DB};";
   ${MYSQL} -e "CREATE DATABASE ${DB};";
-  cat ${DB}-${VERSION}.dump | ${MYSQL} ${DB}
+  echo "importing ${DB}";
+  cat ${HGMD_DIR}/${DB}-${VERSION}.dump | ${MYSQL} ${DB}
 done
 
 #add indices
+echo "indexing hgmd_pro.hg19_coords";
 ${MYSQL} -e "USE hgmd_pro;
 ALTER TABLE hgmd_pro.hg19_coords
 ADD INDEX IX_chromosome (chromosome ASC)
@@ -42,6 +59,7 @@ ADD INDEX IX_chromosome (chromosome ASC)
 , ADD INDEX IX_coordEND (coordEND ASC);";
 
 #stored procedures
+echo "creating hgmd_pro.coord2hgmd stored procedure";
 ${MYSQL} -e "USE hgmd_pro;
 DROP procedure IF EXISTS hgmd_pro.coord2hgmd;
 DELIMITER \$\$
@@ -67,6 +85,7 @@ or hg19_coords.coordEND BETWEEN  coordSTART and coordEND);
 END\$\$
 DELIMITER ;";
 
+echo "creating hgmd_pro.gene2hgmd_disease stored procedure";
 ${MYSQL} -e "USE hgmd_pro;
 DROP procedure IF EXISTS gene2hgmd_disease;
 DELIMITER \$\$
@@ -79,13 +98,16 @@ END\$\$
 DELIMITER ;";
 
 #permissions
-${MYSQL} -e "DROP USER 'hgmduser'@'%';"
-${MYSQL} -e "CREATE USER 'hgmduser'@'%' IDENTIFIED BY 'mootation';"
-${MYSQL} -e "DROP USER 'vax'@'%';"
-${MYSQL} -e "CREATE USER 'hgmd'@'%' IDENTIFIED BY 'hgmd';"
+echo "setting permissions";
 for DB in {hgmd_pro,hgmd_views,hgmd_snp,hgmd_phenbase}; do
   ${MYSQL} -e "GRANT SELECT, INSERT, UPDATE, CREATE TEMPORARY TABLES ON ${DB}."'*'" TO 'hgmduser'@'%' IDENTIFIED BY 'mootation';";
   ${MYSQL} -e "GRANT SELECT, EXECUTE ON ${DB}."'*'" TO 'vax'@'%' IDENTIFIED BY 'vax';";
 done
+
+#test
+echo "testing hgmd_pro.gene2hgmd_disease";
+$(which mysql) -h ${HOST} --user=vax -P ${PORT} -pvax -e "CALL hgmd_pro.gene2hgmd_disease('EXOSC3');";
+echo "testing hgmd_pro.coord2hgmd";
+$(which mysql) -h ${HOST} --user=vax -P ${PORT} -pvax -e "CALL hgmd_pro.coord2hgmd('9', 37780792, 37785040);";
 
 
